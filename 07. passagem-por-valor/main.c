fe5bddd6d9d6f39fd5086dad714046b1e3aa5e18
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-06-18 19:42:56 -0300

    funcoes com passagem por valor

--- 07. passagem-por-valor/main.c
+++ 07. passagem-por-valor/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+int soma(int x, int y){ // recebe as copias, agora x e y nao tem relacao com a e b
+    int z = x + y;
+    return z; // devolve o valor no endereco de z
+}
 
-void main(){
-     
+int main(){
+    int a = 10; // coloca 10 no endereco de a
+    int b = 20;// coloca 20 no endereco de b
+
+    int c = soma(a,b); //copia o valor de a e b para soma e recebe uma copia do retorno
+    return 0;
 }
