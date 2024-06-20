fc8c761b46819d75555865d890142c2c9156254c
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-06-20 08:50:34 -0300

    Exercícios de funções

--- 09. exercicios - funcoes/ex1.c
+++ 09. exercicios - funcoes/ex1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 void swap(int *x, int *y){
-
+    int aux = *x;
+    *x = *y;
+    *y = aux;
 }
 
-/*void main(){
-}*/
+void main(){
+    int a = 1; 
+    int b = 0;
+    swap(&a,&b);
+    printf("%d | %d", a,b);
+}
