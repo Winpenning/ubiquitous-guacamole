d2309978bdee9c267bad59b1d6f1ad02f7d29fb1
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-06-23 10:46:24 -0300

    Exemplos de alocacao e desalocacao de vetores

--- 13. exercicios-vetores/main.c
+++ 13. exercicios-vetores/main.c
@@ -12,16 +12,28 @@ void imprimir_vetor(const int *vetor, int n){
     }
 }
 
+void desalocar_vetor(int **vetor){
+    free(*vetor);
+    printf("\nvetor: %p", vetor);
+    *vetor = NULL;
+    printf("\nvetor: %p", vetor);
+}
+
 void main(){
     int vetor_estatico[5] = {1,2,3,4,5,};
     imprimir_vetor(vetor_estatico,5);
     incrementador(vetor_estatico, 5);
     imprimir_vetor(vetor_estatico,5);
-
     puts("");
     int *vetor_dinamico = (int *) malloc(5*sizeof(int)); // alocado
     imprimir_vetor(vetor_dinamico,5);
     incrementador(vetor_dinamico,5);
     imprimir_vetor(vetor_dinamico,5);
 
+
+    printf("\n\n### ANTES DA DESALOCACAO ###");
+    printf("\n&vetor = %p | vetor = %p", &vetor_dinamico, vetor_dinamico);
+    desalocar_vetor(&vetor_dinamico);
+    printf("\n### DEPOIS DA DESALOCACAO ###");
+    printf("\n&vetor = %p | vetor = %p", &vetor_dinamico, vetor_dinamico);
 }
