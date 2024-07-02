e66963d8f9701ed9f58cdbf86da9aac6a3f7f9a1
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-07-02 17:33:11 -0300

    Mais alterações

--- 17. exercicicos-matrizes/ex2.c
+++ 17. exercicicos-matrizes/ex2.c
@@ -11,14 +11,23 @@ int main(){
     // base da matriz
     int ***m = (int***)malloc(z*sizeof(int **));
 
-    for(int i = 0; i < x; i++){
+    for(int i = 0; i < z; i++){
         m[i] = (int **)malloc(x*sizeof(int *));
-        for(int j = 0; j < y; j++){
+        for(int j = 0; j < x; j++){
             m[i][j] = (int *)malloc(y*sizeof(int));
         }
     }
-
-    m[0][1][1] = 1;
-    printf("%d",m[0][1][1]);
+    int count = 12;
+    for(int i = 0; i < z; i++){
+        printf("\n\n&m[%d] = %p | m[%d] = %p\n",i,&m[i],i,m[i]);
+        for(int j = 0; j < x; j++){
+            printf("\n&m[%d][%d] = %p | m[%d][%d] = %p",i,j,&m[i][j],i,j,m[i][j]);
+            for(int k = 0; k < y; k++){
+                m[i][j][k] = count;
+                count++;
+                printf("\n&m[%d][%d][%d] = %p | m[%d][%d][%d] = %d",i,j,k,&m[i][j][k],i,j,k,m[i][j][k]);
+            }
+        }
+    }
 
 }
